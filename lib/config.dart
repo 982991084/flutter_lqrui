867eca19744d60ea03261e95825cb46dfe8953ed
lqrui <982991084@qq.com>
2019-10-10 15:26:22 +0800

    添加颜色

--- lib/config.dart
+++ lib/config.dart
@@ -8,6 +8,18 @@ export './form/form_page.dart';
 import './flutter_lqrui.dart';
 
 class Lqr {
+  static const double edgeW = 10;
+  static const double imageListW = 110;
+  static const double imageListH = 90;
+  static const Color bgColor = Color(0xFFF1F1F1);
+  static const Color primaryColor = Color(0xFF325ffe);
+  static const Color colorText1 = Color(0xFFb9b8b8);
+  static const Color colorText2 = Color(0xFF999999);
+  static const Color colorText3 = Color(0xFF666666);
+  static const Color colorText4 = Color(0xFF333333);
+  static const Color dividerColor = Color(0xFFeeeeee);
+  static const List<BoxShadow> boxShadow = [BoxShadow(color: Color(0x38AAAAAA), offset: Offset(2.0, 2.0), blurRadius: 3.0), BoxShadow(color: Color(0x38AAAAAA), offset: Offset(-1.0, -1.0), blurRadius: 5.0)];
+
   // Edge++++++++++++++++++++++++++++++++++++++++++++
   edge({double left = 0, double top = 0, double right = 0, double bottom = 0}) => EdgeInsets.fromLTRB(ScreenUtil.getInstance().setWidth(left), ScreenUtil.getInstance().setWidth(top), ScreenUtil.getInstance().setWidth(right), ScreenUtil.getInstance().setWidth(bottom));
   edgeA(double style) => EdgeInsets.all(ScreenUtil.getInstance().setWidth(style));
